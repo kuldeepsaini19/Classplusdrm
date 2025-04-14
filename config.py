22b948bcc1dbba69522814fca0a6f04d49baa9c6
kuldeepsaini19 <138150992+kuldeepsaini19@users.noreply.github.com>
2025-04-14 13:11:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,15 +10,15 @@ PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 #OWNER = int(os.environ.get("OWNER", 7827463899))
 
-#LOG = -1002159628443,
+#LOG = -1002329830617,
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[7827463899]
-    #for x in (os.environ.get("ADMINS", "7827463899").split()):
+    #ADMINS=[7944235618]
+    #for x in (os.environ.get("ADMINS", "7944235618").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
